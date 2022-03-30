bd9828784292e608e1240aebeeed3e62bd4a53b9
eeun <eeun95@gmail.com>
2022-03-30 16:12:20 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,10 +2,9 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
@@ -18,17 +17,28 @@ public class SpringConfig {
     }
      */
 
+    /*
     private EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+     */
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    /*
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
@@ -36,4 +46,6 @@ public class SpringConfig {
         //return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
     }
+
+     */
 }
